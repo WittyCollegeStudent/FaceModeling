359f663faaffcc81a437cfd856b703acafc87956
weianyang <wittycollegestudent@gmail.com>
2017-09-15 02:40:23 -0700

    chang to html

--- src/main/java/com/qianbo/facemodeling/IndexController.java
+++ src/main/java/com/qianbo/facemodeling/IndexController.java
@@ -2,12 +2,20 @@ package com.qianbo.facemodeling;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
 public class IndexController {
-    @RequestMapping(path={"/upload"})
-    public String upload() {
-        return new String("index/upload");
+    @RequestMapping(path={"/index"})
+    public String index() {
+        return "index";
+    }
+    @RequestMapping(path={"history"})
+    public ModelAndView history() {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("name","this is name");
+        mav.setViewName("history");
+        return mav;
     }
 }
